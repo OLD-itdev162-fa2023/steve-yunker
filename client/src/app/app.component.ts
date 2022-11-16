780f197d96679158a8c9f7a804939b22664bc4b2
steve yunker <yunkers@gmatc.matc.edu>
2022-11-15 18:38:48 -0600

    added routes

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,17 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'client';
-  posts: any;
+  title = 'BlogBox';
   
-  constructor(private http: HttpClient) {
+  
+  constructor() {
     
   }
 
   ngOnInit(): void {
-    this.http.get('http://localhost:5000/api/posts').subscribe(
-      response => { this.posts = response; },
-      error => { console.log(error) }
-    );
+    
   }
 }
--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  posts: any;
+  
+  constructor(private http: HttpClient) {
+    
+  }
 
   ngOnInit(): void {
+    this.http.get('http://localhost:5000/api/posts').subscribe(
+      response => { this.posts = response; },
+      error => { console.log(error) }
+    );
   }
 
 }
