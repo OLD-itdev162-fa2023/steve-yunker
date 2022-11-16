bab67a93b34ca0a4485b0a21985b2c0a6a57f140
steve yunker <yunkers@gmatc.matc.edu>
2022-11-15 18:54:28 -0600

    added create post

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,6 +5,9 @@ import {MatCardModule} from '@angular/material/card';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,12 +16,14 @@ import { HomeComponent } from './home/home.component';
 import { ViewPostComponent } from './view-post/view-post.component';
 import { CreatePostComponent } from './create-post/create-post.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ViewPostComponent,
     CreatePostComponent
+    
   ],
   imports: [
     BrowserModule,
@@ -28,7 +33,10 @@ import { CreatePostComponent } from './create-post/create-post.component';
     MatCardModule,
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
